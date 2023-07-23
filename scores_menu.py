ae8382f308dd8d20e0bfaf4dfb05670a1ce0381b
Huang-Yuanhang <140174372+Huang-Yuanhang@users.noreply.github.com>
2023-07-23 22:19:57 +0800

    Update scores_menu.py

--- scores_menu.py
+++ scores_menu.py
@@ -1,20 +1,20 @@
-import scores
+import score
 
 
-def get_valid_score():
+def get_valid_scores():
     while True:
         try:
-            score = float(input("Enter your score (0-100): "))
-            if 0 <= score <= 100:
-                return score
+            scores = float(input("Enter your score (0-100): "))
+            if 0 <= scores <= 100:
+                return scores
             else:
                 print("Score must be between 0 and 100. Please try again.")
         except ValueError:
             print("Invalid input. Please enter a valid score (0-100).")
 
 
-def print_stars(score):
-    print("Stars: " + "*" * int(score))
+def print_stars(scores):
+    print("Stars: " + "*" * int(scores))
 
 
 def main():
@@ -29,11 +29,11 @@ def main():
         choice = input("Please select a menu option (enter the first letter): ").strip().lower()
 
         if choice == 'g':
-            user_score = get_valid_score()
+            user_score = get_valid_scores()
         elif choice == 'p':
             try:
                 if user_score is not None:
-                    result = scores.get_score_result(user_score)
+                    result = score.get_score_result(user_score)
                     print("Your Result:", result)
                 else:
                     print("You haven't entered a valid score yet. Please choose (G)et valid score to enter your score.")
